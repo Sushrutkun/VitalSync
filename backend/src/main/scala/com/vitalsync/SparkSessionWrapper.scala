e34a42c36a843a6458aea51e77491b68327a56ed
sushrutathawale <sushrutathawale1509@gmail.com>
2026-05-30 02:58:23 +0530

    feat: update Spark job configuration and dependencies for improved functionality

--- backend/src/main/scala/com/vitalsync/SparkSessionWrapper.scala
+++ backend/src/main/scala/com/vitalsync/SparkSessionWrapper.scala
@@ -1,13 +1,22 @@
 package com.vitalsync
 
 import org.apache.spark.sql.SparkSession
+import org.apache.hadoop.security.UserGroupInformation
 
 trait SparkSessionWrapper {
+
+    System.setProperty("java.security.auth.useSubjectCredsOnly", "false")
+    System.setProperty("HADOOP_USER_NAME", "spark")
+
+    UserGroupInformation.setLoginUser(
+      UserGroupInformation.createRemoteUser("spark")
+    )
 
     val spark: SparkSession = SparkSession
       .builder()
       .appName("Build a DataFrame from Scratch")
       .master("local[*]")
+      .config("spark.driver.host", "127.0.0.1")
       .getOrCreate()
 
 }
